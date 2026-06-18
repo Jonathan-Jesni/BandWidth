c0b2473196ab85a4ec7364b193406c8d93a9c58c
JONATHAN JESNI MANISSERY <83708080+Jonathan-Jesni@users.noreply.github.com>
2026-06-18 21:28:04 +0530

    fix: handle empty scores list in calculate_average_score

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 def calculate_average_score(scores):
     """Calculates the average of a list of scores."""
-    # BUG: If scores is empty, this throws a ZeroDivisionError!
+    if not scores:
+        return 0.0
     return sum(scores) / len(scores)
