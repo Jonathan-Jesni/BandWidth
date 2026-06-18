085c5814e4ce811967012b0815a0366904dfa1c4
JONATHAN JESNI MANISSERY <83708080+Jonathan-Jesni@users.noreply.github.com>
2026-06-18 23:51:41 +0530

    Fix calculation in calculate_average_score to use division for averaging.

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
 def calculate_average_score(scores):
     """Return the average of a list of scores."""
-    # BUG: This should be division, not multiplication!
-    return sum(scores) * len(scores)
+    return sum(scores) / len(scores)
