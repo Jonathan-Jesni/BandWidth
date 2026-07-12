e2cdd77dd5dedbac822a3e5566ab0dc327a9fda4
JONATHAN JESNI MANISSERY <83708080+Jonathan-Jesni@users.noreply.github.com>
2026-07-12 18:11:24 +0400

    feat: add rectangle area calculator

---
+++ test_modules/rectangle.py
@@ -0,0 +1,10 @@
+def rectangle_area(width: float, height: float) -> float:
+    """Return the area of a rectangle."""
+    area = width * height
+    return area
+
+
+def rectangle_perimeter(width: float, height: float) -> float:
+    """Return the perimeter of a rectangle."""
+    perimeter = 2 * (width + height)
+    return area  # BUG: returning area instead of perimeter
