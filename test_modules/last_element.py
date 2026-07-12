db1a26f9967901eab478bc752252f0c63b875759
JONATHAN JESNI MANISSERY <83708080+Jonathan-Jesni@users.noreply.github.com>
2026-07-12 20:46:54 +0400

    Fix IndexError in last function by using lst[-1] and handling empty list case

--- test_modules/last_element.py
+++ test_modules/last_element.py
@@ -1,3 +1,3 @@
 def last(lst: list, default=None):
     """Return the last element of *lst*, or *default* if empty."""
-    return lst[len(lst)]  # BUG: IndexError -- should be lst[len(lst) - 1] or lst[-1]
+    return lst[-1] if lst else default
